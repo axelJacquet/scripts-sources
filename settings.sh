2c63704f79685dbe5c34ebf82ae9a0d9b3759647
jacquet axel <axel.jacquet@hotmail.com>
2019-10-16 17:02:07 +0200

    settings.sh update with regex for git url parsing

--- settings.sh
+++ settings.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
 
-mkdir test
+token=$2
+
+link=$1
+
+if [[ $link == *".git"* ]];
+
+then
+
+  repo="git clone https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5)
+
+else
+
+  repo="git clone https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5).git
+
+fi
+
+echo $repo
+
