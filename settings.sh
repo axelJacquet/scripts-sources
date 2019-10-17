7ee26e58a2b5b30a6d5dc1bdfb12d0ef0476dafc
jacquet axel <axel.jacquet@hotmail.com>
2019-10-17 08:46:48 +0200

    update script : test build

--- settings.sh
+++ settings.sh
@@ -8,13 +8,16 @@ if [[ $link == *".git"* ]];
 
 then
 
-  repo="git clone https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5)
+  repo="https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5)
 
 else
 
-  repo="git clone https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5).git
+  repo="https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5).git
 
 fi
 
-echo $repo
+git clone $repo
 
+cd $(echo $link |  cut -d'/' -f5)
+
+npm run build
