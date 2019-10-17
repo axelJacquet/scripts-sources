9206f15ea0b5cd631de35be2aceadde38f937a51
jacquet axel <axel.jacquet@hotmail.com>
2019-10-17 13:35:17 +0200

    final script for copy and compil js app

--- settings.sh
+++ settings.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 
 token=$2
-
 link=$1
-
-if [[ $link == *".git"* ]];
-
-then
-
-  repo="https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5)
-
-else
-
-  repo="https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5).git
-
+if [[ -z "$token"  ]]
+  then
+    if [[ $link == *".git"* ]];
+    then
+      repo="https://"$(echo $link |  cut -d'/' -f4 )@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5)
+    else
+      repo="https://"$(echo $link |  cut -d'/' -f4 )@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5).git
+    fi
+  else
+    if [[ $link == *".git"* ]];
+    then
+      repo="https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5)
+    else
+      repo="https://"$(echo $link |  cut -d'/' -f4 ):$token@$(echo $link | cut -d'/' -f3)/$(echo $link |  cut -d'/' -f4)/$(echo $link |  cut -d'/' -f5).git
+    fi
 fi
-
 git clone $repo
-
-cd $(echo $link |  cut -d'/' -f5)
-
+cd $(echo $link |  cut -d'/' -f5 | cut -f1 -d".")
+npm install
 npm run build
